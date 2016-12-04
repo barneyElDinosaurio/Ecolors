95a17aff13e92ebd26052b81c089677280c20bf7
barneyElDinosaurio <marioalzatelopez@gmail.com>
2016-12-04 17:25:32 -0500

    get from firebase the unique id drawing the id in a QR code with picasso

--- app/src/main/java/com/wxyz/root/ecolors/MainActivity.java
+++ app/src/main/java/com/wxyz/root/ecolors/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wxyz.root.ecolors;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -12,26 +13,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import static com.wxyz.root.ecolors.R.id.imageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    FirebaseUser user ;
+    String id;
+    DatabaseReference ref;
+    TextView mTxt,puntaje;
+    ImageView qr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Firebase
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        id = user.getUid().toString();//Get unique user ID
+        //reference id in Database for every user
+        ref = FirebaseDatabase.getInstance().getReference(id);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //SHOW ur inuque ID
+        mTxt = (TextView) findViewById(R.id.texto);
+        mTxt.setText(id);
+        puntaje = (TextView) findViewById(R.id.puntaje);
 
+        qr = (ImageView) findViewById(R.id.qr);
+        Picasso.with(getApplicationContext()).
+                load("https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+id).//load my qr from api.qrserver.com
+                into(qr);//putting image downloaded into my imageView
+
+       
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -40,6 +72,32 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.getValue() != null) {//Si el usuario existe y tiene un valor
+                    puntaje.setText(dataSnapshot.getValue().toString());//lea el puntaje q tiene y pongalo en puntaje
+                }else ref.setValue(0);//si no, hagalo q exista y pongale cero en el puntaje
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
     }
 
     @Override
